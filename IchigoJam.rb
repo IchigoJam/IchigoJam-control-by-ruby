98eb11257c9644d8c18465f866218113150dbb30
Taisuke Fukuno <fukuno@jig.jp>
2019-09-15 06:39:24 +0900

    shorten initialize

--- IchigoJam.rb
+++ IchigoJam.rb
@@ -3,7 +3,7 @@ require 'serialport'
 class IchigoJam
   def initialize
     @port = '/dev/tty.SLAB_USBtoUART'
-    @ser = SerialPort.new(@port, 115200, 8, 1, 0)
+    @ser = SerialPort.new(@port, 115200)
   end
   def puts(s)
     @ser.puts(s)
--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 require 'serialport'
 port = '/dev/tty.SLAB_USBtoUART'
-ser = SerialPort.new(port, 115200, 8, 1, 0)
+ser = SerialPort.new(port, 115200)
 ser.puts("LED1\n")
 p ser.gets
